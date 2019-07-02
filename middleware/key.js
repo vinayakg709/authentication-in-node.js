dd658b4b948eb3bfb5d1f725310b13737ac7d855
vinayakg7091999 <vinayakg709@gmail.com>
2019-07-03 01:52:28 +0530

    removed key.js

--- middleware/key.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    google: {
-        clientId : '85689967573-ilb0v2hhbqhlebmdb5p9c66r3i12bcgp.apps.googleusercontent.com',
-        clientSecret  :'yGQJVAhhPU3SyMw13yjKLKPl'
-    }
-}
