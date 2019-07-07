3d3ab548fbd1b7811f4a7965d55f917bb6026e89
vinayakg709 <vinayakg709@gmail.com>
2019-07-07 18:06:15 +0530

    added oauth2.0

--- middleware/passport-setup.js
+++ middleware/passport-setup.js
@@ -12,6 +12,23 @@ passport.use(
     (accessToken,refreshToken,profile,done) => {
         console.log('callback function is fired');
         console.log(profile);
+
+        Guser.findOne({googleId: profile.id})
+        .then(currentUser => {
+            if(currentUser){
+                console.log('user already exists and user is' + currentUser)
+            }
+            else {
+                const guser = new Guser({
+                    name : profile.displayName,
+                    googleId: profile.id
+                });
+        
+                guser.save().then(result => {
+                    console.log(result)
+                })
+            }
+        })
         
     })
 )
